1e39d0ba997b1929dc81b165be37b007dfd46aaa
yshubert <yshubert@salesforce.com>
2017-11-29 09:59:16 +0200

    add taggun-client

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var restify = require('restify');
 var TaggunClient = require("./taggun-client");
 
+const TAGGUN_API_KEY = "c2614c90d47811e7a18e7df8Ofel9a6e";
 const server = restify.createServer({
   name: 'myapp',
   version: '1.0.0'
@@ -9,14 +10,35 @@ const server = restify.createServer({
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
- 
+
 server.get('/echo/:name', function (req, res, next) {
   res.send(req.params);
   return next();
 });
+
+// Adding route
+server.post('/receipt/file', (req, res, next) =>{
+  /**
+  * Processing request containing multipart/form-data
+  */
+  var uploaded_file = req.files.mySubmittedFile;  // File in form
+
+  //Reading and sending file
+  fs.readFile(uploaded_file.path, {encoding: 'utf-8'}, (err, data)=>{
+      // Returning a JSON containing the file's name and its content
+      res.send({
+          filename: uploaded_file.name,
+          content: data
+      });
+      next()
+  });
+});
+
+
  /*
 server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
 */
-TaggunClient.processReciept();
+var taggunClient = new TaggunClient(TAGGUN_API_KEY);
+taggunClient.makeRequest("./one.jpg");
--- taggun-client.js
+++ taggun-client.js
@@ -2,31 +2,33 @@ var request = require('request');
 var fs = require('fs');
 
 
-const formData = {
-  ipAddress: '219.88.232.1',
-  file: {
-    value: fs.createReadStream(__dirname + "/one.jpg"),
-    options: {
-      filename: "file",
-      contentType: "image/jpeg"
-    }
-  }
+TaggunClient = function(key){
+	this.key = key;
 };
 
-exports.processReciept = function() {
-  console.log("processReciept");
-      // url: 'https://api.taggun.io/api/receipt/v1/verbose/file', formData,
+TaggunClient.prototype.makeRequest = function(imagePath) {
+	const formData = {
+	  file: {
+	    value: fs.createReadStream(imagePath),
+	    options: {
+	      	filename: "file",
+	      	contentType: "image/jpeg"
+	    	}
+  		}
+	};
 
-  request.post({
+	request.post({
     url: 'https://www.taggun.io/api/receipt/v1/file/verbose', formData,
     
-    headers: { apikey: 'c2614c90d47811e7a18e7df8Ofel9a6e' }
+    headers: { apikey: this.key }
   }, function(err, httpResponse, body) {
     if (err) {
       return console.error('upload failed:', err);
     }
     console.log('Upload successful! Server responded with:', body);
-  });  
+  }); 
 
-  
-}
+
+};
+
+module.exports = TaggunClient;
